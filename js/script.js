03b26cd4cf368441e58683449b0f12a6b120a154
Varun Hosakoti <varunhosakoti@outlook.com>
2022-11-22 00:08:51 +0530

    updated in advance version 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var a=prompt("Enter UserName");
+var b=prompt("Enter password");
+if ((a== "varun" && b== "1024") || (a=="praveena" && b== "5123"))
+{
+    window.location.href="./doc/asgmnt/index.html";
+}
+else
+{
+    alert("invalid details");
+    window.location.href="./index.html";
+}
+
+
+<script src="js/script.js"></script>
